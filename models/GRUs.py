be88b14b0b8f73166dbff856be44a70f79f11e1f
Abdelkader Ouared <a_ouared@esi.dz>
2025-03-12 16:40:23 +0100

    Add files via upload

---
+++ models/GRUs.py
@@ -0,0 +1,59 @@
+```python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GRU, Dropout
+from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv('olive_production_dataset.csv')
+data['Date'] = pd.to_datetime(data['Year'].astype(str) + '-' + data['Month'].astype(str) + '-01')
+data.set_index('Date', inplace=True)
+data.fillna(method='ffill', inplace=True)
+
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(data[['Target (Production)']])
+
+train_size = int(len(scaled_data) * 0.8)
+train_data = scaled_data[:train_size]
+test_data = scaled_data[train_size:]
+
+def create_dataset(dataset, time_step=1):
+    X, y = [], []
+    for i in range(len(dataset) - time_step - 1):
+        a = dataset[i:(i + time_step), 0]
+        X.append(a)
+        y.append(dataset[i + time_step, 0])
+    return np.array(X), np.array(y)
+
+time_step = 12
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, y_test = create_dataset(test_data, time_step)
+
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+model = Sequential()
+model.add(GRU(50, return_sequences=True, input_shape=(time_step, 1)))
+model.add(Dropout(0.2))
+model.add(GRU(50, return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=64, verbose=1)
+
+train_predict = model.predict(X_train)
+test_predict = model.predict(X_test)
+
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+
+plt.figure(figsize=(10, 6))
+plt.plot(scaler.inverse_transform(scaled_data), label='Original Data')
+plt.plot(np.arange(time_step, len(train_predict) + time_step), train_predict, label='Train Prediction')
+plt.plot(np.arange(len(train_predict) + (time_step * 2), len(train_predict) + (time_step * 2) + len(test_predict)), test_predict, label='Test Prediction')
+plt.legend()
+plt.show()
+```
